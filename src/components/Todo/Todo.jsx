2fc6bcf662bf94774796218557db9d30f4cea838
ArtemLedovsky <storiedd@gmail.com>
2024-09-24 03:44:57 +0300

    fix

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -4,8 +4,8 @@ const Todo = ({ title, dscr, id, onDelete }) => {
   return (
     <div className={s.item}>
       <div className={s.itemWrapper}>
-        <h2>{title}</h2>
-        <p>{dscr}</p>
+        <h2 className={s.title}>{title}</h2>
+        <p className={s.dscr}>{dscr}</p>
       </div>
       <button className={s.deleteBtn} onClick={() => onDelete(id)}>
         Delete
